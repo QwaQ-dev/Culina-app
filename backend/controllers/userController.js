16fed7456e552f796f9a4884c9a501edbd3127ec
qwaqd <qwaq.dev@gmail.com>
2024-12-18 00:44:14 +0500

    Add password check

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,62 @@
-const db = require('../db/db');
+const db = require("../db/db");
 const dotenv = require("dotenv");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const createHmac = require("crypto")
 dotenv.config();
 
 
 class UsersController{
-    async postUser(req, res) {
+    async signUp(req, res) {
         const {e_mail, username, password} = req.body;
-
-        const hashedPassword = createHmac.createHash('sha256')
+        const secretKey = process.env.SECRET_KEY;
+        const hashedPassword = createHmac.createHash('sha256', secretKey)
                                          .update(password)
                                          .digest('hex')
-        const postUser = await db.query("INSERT INTO dev.users (e_mail, username, password) VALUES  ($1, $2, $3)", [e_mail, username, hashedPassword]) 
-        const user = await db.query("SELECT * FROM dev.users WHERE e_mail = $1", [e_mail]);
-        const jwtSecretKey = process.env.JWT_SECRET_KEY
 
-        let data = {
-            time: Date(),
-            user: user.rows[0],
+        const user = await db.query("SELECT * FROM dev.users WHERE username = $1", [username]);
+        if(user.rows.length >= 1) {
+            res.status(500).json({message: "User with this name is already exists"});
+        } else {
+            const postUser = await db.query("INSERT INTO dev.users (e_mail, username, password) VALUES  ($1, $2, $3)", [e_mail, username, hashedPassword]) ;
+            
+            const tokenData = {
+                time: Date(),
+                user: user.rows[0].username,
+            };
+
+            const token = jwt.sign(tokenData, secretKey);
+            res.status(200).json({'JWT': token});
         }
-        let token = jwt.sign(data, jwtSecretKey)
 
-        res.status(200).json({'JWT': token})
     }
 
+    async signIn(req, res) {
+        const { username, password } = req.body;
+    
+        const user = await db.query("SELECT * FROM dev.users WHERE username = $1", [username]);
+        if (user.rows.length === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        const storedHashedPassword = user.rows[0].password;
+    
+        const secretKey = process.env.SECRET_KEY;
+        const hashedPassword = createHmac.createHash('sha256', secretKey)
+                                            .update(password)
+                                            .digest('hex');
+    
+        if (hashedPassword !== storedHashedPassword) {
+            return res.status(401).json({ message: "Incorrect password" });
+        }
 
-    async getUser(req, res) {
-        const {id} = req.params;
+        const tokenData = {
+            time: Date(),
+            user: user.rows[0].username,
+        };
 
-
-
-
-        res.status(200).json({"user": user.rows[0]})
+        const token = jwt.sign(tokenData, secretKey);
+    
+        return res.status(200).json({ JWT: token });
+    
     }
 
 }
--- backend/routes/mainRouter.js
+++ backend/routes/mainRouter.js
@@ -3,8 +3,7 @@ const UsersController = require("../controllers/userController");
 const router = new Router();
 
 
-router.post('/user', UsersController.postUser);
-
-router.get('/users/:id', UsersController.getUser);
+router.post('/sign-up', UsersController.signUp);
+router.post('/sign-in', UsersController.signIn);
 
 module.exports = router;
