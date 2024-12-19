97ef8a24f144bd833fad43bd004eeff3c22ccc11
Danil <danil.ignashov@student.ukf.sk>
2024-12-19 20:42:35 +0100

    working sign-in sign-up auth redirecting to main page

---
+++ frontend/src/app/(app)/logged-layout.js
@@ -0,0 +1,13 @@
+export default function Loggedlayout({children}){
+    return(
+        <html lang="en">
+            <body>
+                <div>
+                    <h1>THIS IS LAYOUT</h1>
+                    {children}
+                </div>
+            </body>
+
+        </html>
+    );
+}
---
+++ frontend/src/app/api/auth/signup.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export async function handlerSignUp(e_mail, username, password){
+    try{
+        const response = await axios.post("http://localhost:8080/sign-up", {
+            e_mail,
+            username,
+            password,
+        })
+        return response
+
+    }catch(error){
+        throw error.response?.data?.message || "User already exists";
+    }
+}
+
+export async function handlerSignIn(username, password){
+    try{
+        const response = await axios.post("http://localhost:8080/sign-in",{
+            username,
+            password,
+        })
+        return response
+    }catch(error){
+        throw "Wrong username or password";
+    }
+}
