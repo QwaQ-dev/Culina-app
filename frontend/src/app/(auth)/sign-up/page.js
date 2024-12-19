75dbb68d9f7630b9100ab5ccead0c4ca2c3c988b
Danil <danil.ignashov@student.ukf.sk>
2024-12-19 20:47:12 +0100

    Update page.js

--- frontend/src/app/(auth)/sign-up/page.js
+++ frontend/src/app/(auth)/sign-up/page.js
@@ -51,12 +51,6 @@ export default function SignIn() {
                 </div>
                 <div className="ride-side sm:w-1/2 mt-24 ">
                     <h2 className="text-center text-2xl sm:text-5xl text-nowrap font-bold">Sign Up</h2>
-<<<<<<< Updated upstream
-                    <form action="POST " className="flex flex-col text-xs sm:text-xl items-center gap-5 mt-10">
-                        <input type="text" placeholder="E-mail" name="email"  className="py-2 px-4 border-nedoblack border-2 rounded-lg"/>
-                        <input type="text" placeholder="Username" name="name"  className="py-2 px-4 border-nedoblack border-2 rounded-lg"/>
-                        <input type="text" placeholder="Password" name = "password" className="py-2 px-4 border-nedoblack border-2 rounded-lg"/>
-=======
                     <form className="flex flex-col text-xs sm:text-xl items-center gap-5 mt-10"
                         onSubmit={handleSubmit}
                     >
@@ -81,7 +75,6 @@ export default function SignIn() {
                             placeholder="Password" 
                             name = "password" 
                             className="py-2 px-4 border-nedoblack border-2 rounded-lg" required/>
->>>>>>> Stashed changes
                         <button className="relative inline-block py-2 px-6 border-2  rounded-xl border-nedoblack text-nowrap text-xl text-nedowhite bg-no-repeat hover:bg-gradient-to-hover bg-center bg-nedoblack transition-colors duration-300 hover:text-nedoblack hover:animate-fill-center hover:border-nedoblack">
                             Sign Up
                         </button>
