1231db06d3c5f903cfe752f10610cc7cdc3526bd
qwaqd <qwaq.dev@gmail.com>
2024-12-27 00:07:32 +0500

    Add updating user password, username and user sex

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,7 +8,7 @@ dotenv.config();
 class UsersController{
     async signUp(req, res) {
         const {e_mail, username, password} = req.body;
-        
+
         const secretKey = process.env.SECRET_KEY;
         const hashedPassword = createHmac.createHash('sha256', secretKey)
                                          .update(password)
@@ -62,6 +62,59 @@ class UsersController{
         const token = jwt.sign(tokenData, secretKey);
     
         return res.status(200).json({ JWT: token });
+    }
+
+    async newUsername(req, res){
+        const { prevUsername, newUsername } = req.body;
+
+        try {
+            await db.query("UPDATE dev.users SET username = $1 WHERE username = $2",
+                [newUsername, prevUsername]);
+            res.status(200).json({message: "Username has been changed!"});
+        } catch (error) {
+            res.status(500).json({message: "Cannot change username", error: error.message});
+        }
+    }
+
+    async newPassword(req, res){
+        const { username, prevPassword, newPassword } = req.body;
+        const secretKey = process.env.SECRET_KEY;
+        const passwordFromDb = await db.query("SELECT password FROM dev.users WHERE username = $1", [username]);
+
+        const hashedPassword = createHmac.createHash('sha256', secretKey)
+                                                    .update(prevPassword)
+                                                    .digest('hex');
+        const newHashedPassword = createHmac.createHash('sha256', secretKey)
+                                                        .update(newPassword)
+                                                        .digest('hex');
+
+        if(passwordFromDb.rows[0].password == hashedPassword) {
+            try {
+                await db.query("UPDATE dev.users SET password = $1 WHERE username = $2",
+                    [newHashedPassword, username]);
+                res.status(200).json({message: "Password has been changed!"});
+            } catch (error) {
+                res.status(500).json({message: "Cannot change user password", error: error.message});
+            }
+        } else {
+            res.status(500).json({message: "Password is incorrect"})
+        }
+    }
+
+    async newSex(req, res){
+        const { username, prevSex, newSex } = req.body;
+
+        if(prevSex === newSex) {
+            res.status(500).json({message: `Your sex is already ${newSex}`});
+        } else {
+            try {
+                await db.query("UPDATE dev.users SET sex = $1 WHERE username = $2",
+                    [newSex, username]);
+                res.status(200).json({message: "Sex has been changed!"});
+            } catch (error) {
+                res.status(500).json({message: "Cannot change user sex", error: error.message});
+            }
+        }
     }
 
 }
--- backend/routes/mainRouter.js
+++ backend/routes/mainRouter.js
@@ -6,10 +6,15 @@ const UploadImages  = require('../multer/UploadImages')
 
 router.post('/sign-up', UserController.signUp);
 router.post('/sign-in', UserController.signIn);
+
+router.post('/profile/username', UserController.newUsername);
+router.post('/profile/password', UserController.newPassword);
+router.post('/profile/sex', UserController.newSex);
+router.get('/profile/receipts/:author', CardController.userReceipts); 
+
 router.post('/create-receipt', UploadImages.uploadReceiptImages, CardController.createReceipt);
 
 router.get('/dashboard/receipts', CardController.allReceipts);
-router.get('/dashboard/receipt/:id', CardController.oneReceipt); 
-router.get('/profile/receipts/:author', CardController.userReceipts);   
+router.get('/dashboard/receipt/:id', CardController.oneReceipt);   
 
 module.exports = router;
