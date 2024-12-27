2a34eb38d6f32a804bc385cca39045ab11d2fcb1
qwaqd <qwaq.dev@gmail.com>
2024-12-27 22:50:25 +0500

    Update routes

--- backend/routes/mainRouter.js
+++ backend/routes/mainRouter.js
@@ -1,20 +1,11 @@
 const Router = require("express");
-const UserController = require("../controllers/UserController");
-const CardController = require("../controllers/ReceiptController");
 const router = new Router();
-const UploadImages  = require('../multer/UploadImages')
+const ProfileRouter = require("./profileRouter");
+const UserRouter = require("./userRouter");
+const DashboardRouter = require("./dashboardRouter");
 
-router.post('/sign-up', UserController.signUp);
-router.post('/sign-in', UserController.signIn);
-
-router.post('/profile/username', UserController.newUsername);
-router.post('/profile/password', UserController.newPassword);
-router.post('/profile/sex', UserController.newSex);
-router.get('/profile/receipts/:author', CardController.userReceipts); 
-
-router.post('/create-receipt', UploadImages.uploadReceiptImages, CardController.createReceipt);
-
-router.get('/dashboard/receipts', CardController.allReceipts);
-router.get('/dashboard/receipt/:id', CardController.oneReceipt);   
+router.use('/profile', ProfileRouter);
+router.use('/dashboard', DashboardRouter);
+router.use('/user', UserRouter);
 
 module.exports = router;
