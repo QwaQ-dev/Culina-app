a0c2393abc221a3b5bb1e3a6010362c947c7fa43
qwaqd <qwaq.dev@gmail.com>
2024-12-28 01:26:01 +0500

    Backup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,32 +1,35 @@
 const db = require("../db/db");
+const { User } = require("../db/db");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
-const createHmac = require("crypto")
+const bcrypt = require("bcrypt");
 dotenv.config();
 
+
+const generateJwt = (username, role, sex) => {
+    return jwt.sign(
+        {username, role, sex}, 
+        process.env.SECRET_KEY, 
+        {expiresIn: "12h"}
+    );
+};
 
 class UsersController{
     async signUp(req, res) {
         const {e_mail, username, password} = req.body;
 
-        const secretKey = process.env.SECRET_KEY;
-        const hashedPassword = createHmac.createHash('sha256', secretKey)
-                                         .update(password)
-                                         .digest('hex');
+        const hashedPassword = await bcrypt.hash(password, 5);
 
         try {
             const user = await db.query("SELECT * FROM dev.users WHERE username = $1", [username]);
+
             if(user.rows.length >= 1) {
                 return res.status(500).json({message: "User with this name is already exists"});
             } else {
                 await db.query("INSERT INTO dev.users (e_mail, username, password, role, sex) VALUES ($1, $2, $3, $4, $5)", 
                                 [e_mail, username, hashedPassword, 'Basic', 'male']) ;
 
-                const token = jwt.sign(
-                    {username, role: "Basic", sex: "male"}, 
-                    secretKey, 
-                    {expiresIn: "12h"}
-                );
+                const token = generateJwt(username, 'Basic', 'male');
 
                 return res.status(200).json({'JWT': token});
             }
@@ -40,35 +43,21 @@ class UsersController{
         const { username, password } = req.body;
     
         const user = await db.query("SELECT * FROM dev.users WHERE username = $1", [username]);
+        const userRows = user.rows[0];
 
         if (user.rows.length === 0) {
             return res.status(404).json({ message: "User not found" });
         }
 
-        const storedHashedPassword = user.rows[0].password;
-        const secretKey = process.env.SECRET_KEY;
-        const hashedPassword = createHmac.createHash('sha256', secretKey)
-                                            .update(password)
-                                            .digest('hex');
-        
-        if (hashedPassword !== storedHashedPassword) {
-            return res.status(401).json({ message: "Incorrect password" });
-        }
+        const comparePassword = await bcrypt.compare(password, userRows.password);
 
-        const tokenData = {
-            user: JSON.stringify({
-                "username": user.rows[0].username,
-                "role": user.rows[0].role,
-                "sex": user.rows[0].sex,
-            }),
-        };
-
-        const token = jwt.sign(
-            tokenData, 
-            secretKey,
-            {expiresIn: "12h"});
+        if(comparePassword) {
+            const token = generateJwt(userRows.username, userRows.role, userRows.sex);
     
-        return res.status(200).json({ JWT: token });
+            return res.status(200).json({ JWT: token });
+        } else {
+            return res.status(500).json({message: "Password is incorrect"});
+        };
     }
 
     async newUsername(req, res){
@@ -85,26 +74,21 @@ class UsersController{
 
     async newPassword(req, res){
         const { username, prevPassword, newPassword } = req.body;
-        const secretKey = process.env.SECRET_KEY;
-        const passwordFromDb = await db.query("SELECT password FROM dev.users WHERE username = $1", [username]);
+        try {
+            const password = await db.query("SELECT password FROM dev.users WHERE username = $1", [username]);
+            const comparePassword = await bcrypt.compare(prevPassword, password.rows[0].password);
 
-        const hashedPassword = createHmac.createHash('sha256', secretKey)
-                                                    .update(prevPassword)
-                                                    .digest('hex');
-        const newHashedPassword = createHmac.createHash('sha256', secretKey)
-                                                        .update(newPassword)
-                                                        .digest('hex');
+            if(comparePassword){
+                const hashedNewPassword = await bcrypt.hash(newPassword, 5);
 
-        if(passwordFromDb.rows[0].password == hashedPassword) {
-            try {
                 await db.query("UPDATE dev.users SET password = $1 WHERE username = $2",
-                    [newHashedPassword, username]);
+                    [hashedNewPassword, username]);
                 return res.status(200).json({message: "Password has been changed!"});
-            } catch (error) {
-                return res.status(500).json({message: "Cannot change user password", error: error.message});
+            } else {
+                return res.status(500).json({message: "Password is incorrect"});
             }
-        } else {
-            return res.status(500).json({message: "Password is incorrect"})
+        } catch (error) {
+            return res.status(500).json({message: "Cannot change user password", error: error.message});
         }
     }
 
---
+++ backend/models/models.js
@@ -0,0 +1,32 @@
+const sequelize = require("../db/db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define('users', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    e_mail: {type: DataTypes.STRING, unique: true},
+    username: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue:"Basic"},
+    sex: {type: DataTypes.STRING, defaultValue:"male"}
+});
+
+const Receipts = sequelize.define('receipts', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true},
+    descr: {type: DataTypes.STRING},
+    diff: {type: DataTypes.STRING},
+    filters: {type: DataTypes.JSONB},
+    imgs: {type: DataTypes.JSONB},
+    author: {type: DataTypes.STRING}
+});
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false}
+});
+
+module.exports = {
+    User,
+    Receipts,
+    Rating
+}
