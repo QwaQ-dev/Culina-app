99bfd9559a5898374ae883aa5e9fa233eb6f6874
qwaqd <qwaq.dev@gmail.com>
2025-01-04 00:42:49 +0500

    Add docker. Now you can start backend with only docker compose up -d

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const apiRouter = require("./routes/mainRouter");
 const sequelize = require("./db/db");
 const models = require("./models/models")
+const uploadImagesMiddleware = require("./middleware/uploadImagesMiddleware");
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-const uploadImagesMiddleware = require("./middleware/uploadImagesMiddleware");
 
 dotenv.config();
 
 app.use(cors());
-
 app.use(express.json());
 app.use('/uploads/:author/receipts-imgs', uploadImagesMiddleware);
 app.use('/api/v1', apiRouter)
