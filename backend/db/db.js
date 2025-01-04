008d9a3f5b33d1d763ac43d1d955398b5a8718a0
qwaqd <qwaq.dev@gmail.com>
2025-01-05 01:25:20 +0500

    add host

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize('Culina-app', 'postgres', 'postgres', {
+    host: 'ciluna-db',
     dialect: 'postgres',
     port: 5432,
     logging: false, 
