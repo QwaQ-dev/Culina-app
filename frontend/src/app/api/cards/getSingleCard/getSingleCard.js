97aa57fc4529b5d2f2b7315b28b0614217ac06ce
qwaqd <qwaq.dev@gmail.com>
2025-03-13 09:21:05 +0500

    Correct routes at frontend

--- frontend/src/app/api/cards/getSingleCard/getSingleCard.js
+++ frontend/src/app/api/cards/getSingleCard/getSingleCard.js
@@ -5,7 +5,7 @@ import axios from "axios"
 
 export async function getSingleCard(id){
     try{
-        const response = await axios.get(`http://localhost:8080/api/v1/dashboard/receipt/${id}`)
+        const response = await axios.get(`http://localhost:8080/dashboard/receipt/${id}`)
         return response
     }catch(err){
         throw "Error while getting single card"
